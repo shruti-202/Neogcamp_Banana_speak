d6c225222d6e68f3469c01e66fd1142aa95d6143
shruti-202 <110720732+shruti-202@users.noreply.github.com>
2022-11-30 20:35:31 +0530

    commiting

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log("Hi Software Developer");
+
+var userInput = prompt("What's your Name!"); //prompt is use to take input
+var wlcmMessage = userInput + "! Welcome to Neogcamp ! ";
+alert(wlcmMessage); //alert is use to display the content
+
+var btnTranslate = document.querySelector("#btn-translate"); //This tells the browser to fetch btn-translate id...
+btnTranslate.addEventListener("click", function clickEventHandler() {
+  console.log("Clicked!");
+});
+
+// function clickEventHandler(){....This way we can extra the function
+// console.log("Clicked!");
+// }
+// btnTranslate.addEventListener("click",clickEventHandler);....ANd from here we can call back function
+
+var inputText = document.querySelector("#text-input");
+inputText.addEventListener("click", function clickEventHandler() {
+  console.log("input", inputText.value);
+});
