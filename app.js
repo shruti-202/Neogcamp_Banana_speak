a1c36f7c8e6cacd7847d22f4c2160e88a9633a66
shruti-202 <110720732+shruti-202@users.noreply.github.com>
2022-12-02 17:53:46 +0530

    feat:Added new translated feature

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ alert(wlcmMessage); //alert is use to display the content
 
 var btnTranslate = document.querySelector("#btn-translate"); //This tells the browser to fetch btn-translate id...
 btnTranslate.addEventListener("click", function clickEventHandler() {
-  // console.log("Clicked!");
-  outputText.innerText = "zyzyzyyzyzyz " + inputText.value;
+  console.log("Clicked!");
+  // outputText.innerText = "zyzyzyyzyzyz " + inputText.value;
+  var userInput = inputText.value; //Reading Input
+  fetch(translatedUrl(userInput)) //Calling the server
+    .then((response) => response.json())
+    .then((json) => {
+      var translateText = json.contents.translated;
+      outputText.innerText = translateText;
+    })
+
+    .catch(errorHandler);
 });
 
 // function clickEventHandler(){....This way we can extra the function
@@ -23,3 +32,18 @@ inputText.addEventListener("click", function clickEventHandler() {
 var outputText = document.querySelector("#output");
 // outputText.innerText = "Shruti ,The Software Engineer!";
 // console.log(outputText);shh
+
+// var serverUrl = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+var serverUrl = "https://api.funtranslations.com/translate/minion.json"; //Minion server
+function translatedUrl(text) {
+  return (
+    "https://api.funtranslations.com/translate/minion.json" +
+    "?" +
+    "text=" +
+    text
+  );
+}
+//To handler errors
+function errorHandler(error) {
+  console.log("Server is down", error);
+}
